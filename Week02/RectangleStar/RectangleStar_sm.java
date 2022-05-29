4c0082331a01a8fa99dce9fc0d3bb2650e7e2afe
Seonmi-Hwang <50273050+Seonmi-Hwang@users.noreply.github.com>
2022-05-29 11:56:48 +0900

    [Solve] ✍ programmers_level1.직사각형 별찍기

---
+++ Week02/RectangleStar/RectangleStar_sm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RectangleStar {
+
+    public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int m = s.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
